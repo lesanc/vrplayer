f540826571ac9296494d26afc5a36cad369e8d1d
lesanc <lesanc@126.com>
2017-07-16 15:30:29 +0800

    add google vr sdk

--- app/src/main/java/name/lixianke/demo/VideoPlayActivity.java
+++ app/src/main/java/name/lixianke/demo/VideoPlayActivity.java
@@ -1,15 +1,34 @@
 package name.lixianke.demo;
 
-import android.app.Activity;
 import android.media.MediaPlayer;
+import android.opengl.GLES20;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.VideoView;
 
-public class VideoPlayActivity extends Activity {
+import com.google.vr.sdk.base.Eye;
+import com.google.vr.sdk.base.GvrActivity;
+import com.google.vr.sdk.base.GvrView;
+import com.google.vr.sdk.base.HeadTransform;
+import com.google.vr.sdk.base.Viewport;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+import name.lixianke.vrplayer.R;
+
+public class VideoPlayActivity extends GvrActivity implements GvrView.StereoRenderer {
+    private static final String TAG = "VideoPlayActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        GvrView gvrView = new GvrView(this);
 
         VideoView vv = new VideoView(this);
         vv.setVideoPath("file:" + getIntent().getStringExtra("path"));
@@ -20,6 +39,86 @@ public class VideoPlayActivity extends Activity {
             }
         });
 
-        setContentView(vv);
+        setGvrView(gvrView);
+    }
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i1) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+        loadGLShader(GLES20.GL_VERTEX_SHADER, R.raw.video_vertex);
+        loadGLShader(GLES20.GL_FRAGMENT_SHADER, R.raw.video_fragment);
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
+
+    private int loadGLShader(int type, int rawId) {
+        String source = readRawTextFile(rawId);
+        int shader = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(shader, source);
+        GLES20.glCompileShader(shader);
+
+        // Get the compilation status.
+        final int[] compileStatus = new int[1];
+        GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+        // If the compilation failed, delete the shader.
+        if (compileStatus[0] == 0) {
+            Log.e(TAG, "Error compiling shader: " + GLES20.glGetShaderInfoLog(shader));
+            GLES20.glDeleteShader(shader);
+            shader = 0;
+        }
+
+        if (shader == 0) {
+            throw new RuntimeException("Error creating shader.");
+        }
+
+        return shader;
+    }
+
+    private String readRawTextFile(int resId) {
+        InputStream inputStream = getResources().openRawResource(resId);
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            reader.close();
+            return sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static void checkGLError(String label) {
+        int error;
+        while ((error = GLES20.glGetError()) != GLES20.GL_NO_ERROR) {
+            Log.e(TAG, label + ": glError " + error);
+            throw new RuntimeException(label + ": glError " + error);
+        }
     }
 }
